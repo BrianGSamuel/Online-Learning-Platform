51df5a21c61eb312c04a1a2d1aca618ecffd97a5
Brian.G.Samuel <167592418+BrianGSamuel@users.noreply.github.com>
2025-07-19 08:54:40 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,6 +25,8 @@ import StudentEditProfile from "./features/dashboard/student/StudentEditProfile"
 import ManageLibrary from "./features/dashboard/admin/EBookUpload";
 
 
+import StudentLibrary from "./features/dashboard/student/eLibrary";
+import TeacherLibrary from "./features/dashboard/teacher/eLibrary";
 import PaymentForm from "./features/dashboard/student/PaymentForm";
 import MyClasses from "./features/dashboard/student/MyClasses";
  
@@ -36,7 +38,8 @@ import ClassMaterial from "./features/dashboard/student/ClassMaterials";
 import AvailableClasses from "./features/dashboard/student/AvailableClasses";
 import PaymentHistory from "./features/dashboard/student/PaymentHistory";
 import RefundManagement from "./features/dashboard/admin/RefundManagement";
-
+import RefundRequest from "./features/dashboard/student/RefundRequest";
+import RefundHistory from "./features/dashboard/student/RefundHistory";
 
 import RoleSelection from "./features/auth/RoleSelection";
 
@@ -49,8 +52,8 @@ import StudyPacks from './features/dashboard/student/StudyPacks';
 import PurchasedStudyPacks from './features/dashboard/student/PurchasedStudyPacks';
 import ManageStudyPacks from './features/dashboard/teacher/ManageStudyPacks';
 
-
-
+import StudentMessage from "./features/dashboard/student/StudentMessage";
+import TeacherMessage from "./features/dashboard/teacher/TeacherMessage";
 
 import ManageFAQs from "./features/dashboard/admin/ManageFAQs";
 
@@ -61,28 +64,41 @@ import ManageKnowledgebase from "./features/dashboard/admin/ManageKnowledgebase"
 import KnowledgebaseCategoryPage from "./features/home/KnowledgebaseCategoryPage";
 import ContactMessages from "./features/dashboard/admin/ContactMessages";
 
+import ManageExtensionRequests from "./features/dashboard/teacher/ManageExtensionRequests";
 
+import QuizGenerator from "./features/dashboard/teacher/QuizGenerator";
+import StudentQuiz from "./features/dashboard/student/StudentQuiz";
+import StudentQuizList from "./features/dashboard/student/StudentQuizList";
 
+import DoubtResolver from "./features/dashboard/student/DoubtResolver"
 
+import StudentNotices from "./features/dashboard/student/StudentNotices";
+import TeacherNotices from "./features/dashboard/teacher/TeacherNotices";
 
 import AdminNotices from "./features/dashboard/admin/AdminNotices";
-
+import TeacherNoticesView from "./features/dashboard/teacher/TeacherNoticesView";
 
 import InstituteNoticesView from "./features/dashboard/institute/InstituteNoticesView";
 
+import Report from "./features/dashboard/teacher/Report";
+
+
+import TeacherSupportForm from "./features/dashboard/teacher/TeacherSupportForm";
 import AdminSupportTickets from "./features/dashboard/admin/AdminSupportTickets";
 import AdminSupportTicketDetails from "./features/dashboard/admin/AdminSupportTicketDetails";
 import AdminSupportCategories from "./features/dashboard/admin/AdminSupportCategories";
-
+import TeacherSupportTickets from "./features/dashboard/teacher/TeacherSupportTickets";
+import TeacherSupportTicketDetails from "./features/dashboard/teacher/TeacherSupportTicketDetails";
 
 
 import InstituteSupportForm from "./features/dashboard/institute/InstituteSupportForm";
 import InstituteSupportTickets from "./features/dashboard/institute/InstituteSupportTickets";
 import InstituteSupportTicketDetails from "./features/dashboard/institute/InstituteSupportTicketDetails";
 
+import FeeWaiverForm from "./features/dashboard/student/FeeWaiverForm";
+import FeeWaiverRequests from "./features/dashboard/teacher/FeeWaiverRequests";
 
-
-
+import PersonalizedPath from "./features/dashboard/student/PersonalizedPath";
 
 import TeacherPaymentHistory from "./features/dashboard/teacher/PaymentHistory";
 
@@ -124,10 +140,10 @@ function App() {
             <Route path="/institute/payment-history" element={<ProtectedRoute role="institute"><InstitutePaymentHistory /></ProtectedRoute>} />
 
 
-       
+            <Route path="/student/personalied-path" element={<ProtectedRoute role="student"><PersonalizedPath /></ProtectedRoute>} />
 
-            
-            
+            <Route path="/student/fee-waiver" element={<ProtectedRoute role="student"><FeeWaiverForm /></ProtectedRoute>} />
+            <Route path="/teacher/fee-waiver-requests" element={<ProtectedRoute role="teacher"><FeeWaiverRequests /></ProtectedRoute>} />
 
             <Route path="/institute/support-form" element={<ProtectedRoute role="institute"><InstituteSupportForm /></ProtectedRoute>} />
             <Route path="/institute/support-tickets" element={<ProtectedRoute role="institute"><InstituteSupportTickets /></ProtectedRoute> } />
@@ -138,7 +154,9 @@ function App() {
             <Route path="/admin/support/ticket"  element={ <ProtectedRoute role="admin"><AdminSupportTickets /></ProtectedRoute>} />
             <Route path="/admin/support/ticket/:ticketId" element={ <ProtectedRoute role="admin"><AdminSupportTicketDetails /></ProtectedRoute>} />
             <Route path="/admin/support/categories" element={<ProtectedRoute role="admin"><AdminSupportCategories /></ProtectedRoute>} />
-           
+            <Route path="/teacher/support-ticket" element={<ProtectedRoute role="teacher"><TeacherSupportTickets /></ProtectedRoute> } />
+            <Route  path="/teacher/support/ticket/:ticketId" element={<ProtectedRoute role="teacher"><TeacherSupportTicketDetails /> </ProtectedRoute>} />
+            <Route path="/teacher/support-form" element={<ProtectedRoute role="teacher"><TeacherSupportForm /></ProtectedRoute>} />
                     
                 <Route path="/" element={<HomePage />} />
                 <Route path="/pricing" element={<PricingPage />} />
@@ -168,20 +186,23 @@ function App() {
                 <Route path="/admin/manage-knwoledgebase" element={<ProtectedRoute role="admin">< ManageKnowledgebase/></ProtectedRoute>} />
                 <Route path="/admin/contact-messages" element={<ProtectedRoute role="admin">< ContactMessages/></ProtectedRoute>} />
                 
-                
+                <Route path="/teacher/quiz-generator" element={<ProtectedRoute role="teacher"><QuizGenerator /></ProtectedRoute>} />
+                <Route path="/student/quizlist/quiz/:quizId" element={<ProtectedRoute role="student"><StudentQuiz /></ProtectedRoute>} />
+                <Route path="/student/quizlist" element={<ProtectedRoute role="student"><StudentQuizList /></ProtectedRoute>} />
                 
 
-                
+                <Route path="/student/doubt-resolver" element={<ProtectedRoute role="student"><DoubtResolver /></ProtectedRoute>} />
                 <Route path="/student/dashboard/*" element={<ProtectedRoute role="student"><StudentDashboard /></ProtectedRoute>} />
-              
-                
-
+                <Route path="/student/library" element={<ProtectedRoute role="student"><StudentLibrary /></ProtectedRoute>} />
+                <Route path="/teacher/library" element={<ProtectedRoute role="teacher"><TeacherLibrary  /></ProtectedRoute>} />
                 
                 <Route path="/student/dashboard/subscribe/:classId" element={<PaymentForm />} />
                 <Route path="/student/dashboard/my-classes" element={<MyClasses />} />
                 <Route path="/student/dashboard/my-classes/:classId/materials" element={<ClassMaterial />} />
                 <Route path="/student/dashboard/available-classes" element={<ProtectedRoute role="student"><AvailableClasses /></ProtectedRoute>} />
                 <Route path="/student/dashboard/payment-history" element={<ProtectedRoute role="student"><PaymentHistory /></ProtectedRoute>} /> 
+                <Route path="/student/dashboard/refund-request" element={<ProtectedRoute role="student"><RefundRequest /></ProtectedRoute>} /> {/* New route */}
+                <Route path="/student/dashboard/refund-history" element={<ProtectedRoute role="student"><RefundHistory /></ProtectedRoute>} /> {/* New route */}
                 
 
                 <Route path="/institute/dashboard" element={<ProtectedRoute role="institute"><InstituteDashboard /></ProtectedRoute>} />
@@ -210,7 +231,7 @@ function App() {
 
                 
                 <Route path="/student/dashboard/all-teachers" element={<ProtectedRoute role="student"><ActiveTeachers /></ProtectedRoute>} /> {/* New route */}
-                
+                <Route path="/teacher/extension-requests" element={<ProtectedRoute role="teacher"><ManageExtensionRequests /></ProtectedRoute>} />
                
 <Route path="/teacher/upload-studypack" element={<UploadStudyPack />} />
 <Route path="/student/studypacks" element={<StudyPacks />} />
@@ -218,19 +239,24 @@ function App() {
 
 <Route path="/teacher/manage-studypacks" element={<ManageStudyPacks />} />
 
+<Route path="/student/message" element={<ProtectedRoute role="student"><StudentMessage /></ProtectedRoute>} />
+<Route path="/teacher/message" element={<TeacherMessage />} />
 
 
-
-
+<Route path="/student/notices" element={<ProtectedRoute role="student"><StudentNotices /></ProtectedRoute>} />
+<Route path="/student/notice/:noticeId" element={<ProtectedRoute role="student"><StudentNotices /></ProtectedRoute>} />
+<Route path="/teacher/notices" element={<ProtectedRoute role="teacher"><TeacherNotices /></ProtectedRoute>} />
 
 <Route path="/admin/notices" element={<ProtectedRoute role="admin"><AdminNotices /></ProtectedRoute>}  />
-
+ {/* Teacher Routes */}
+<Route path="/teacher/noticesview" element={<ProtectedRoute role="teacher"><TeacherNoticesView /> </ProtectedRoute>} />
+<Route path="/teacher/noticesview/:noticeId" element={<ProtectedRoute role="teacher"><TeacherNoticesView /></ProtectedRoute> }  />
 
 <Route path="/institute/notices/" element={<ProtectedRoute role="institute"><InstituteNoticesView  /> </ProtectedRoute>} />
 <Route path="/institute/notice/:noticeId" element={<ProtectedRoute role="institute"><InstituteNoticesView /></ProtectedRoute> }  />
 
 
-
+<Route path="/teacher/report" element={<ProtectedRoute role="teacher"> <Report /></ProtectedRoute>} />
 
 
 </Routes>
